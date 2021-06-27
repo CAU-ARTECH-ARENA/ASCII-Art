b2709c9d752a286d9cdda6ad93839aa4c76e0540
802-R <81912810+802-R@users.noreply.github.com>
2021-06-27 23:39:00 +0900

    Update CAU.py

--- artwork/02-CAU/CAU.py
+++ artwork/02-CAU/CAU.py
@@ -5,24 +5,24 @@ print("""
                                                                                                                   //                        //
                                                                                                                   //                        //
                                                                                                                   //         CAUC           //
-									                                                                              //        CAUCAUCA        //
-									                                                                     CAU      //       CAUCAUCAUC       //
-				                                                          CAUCAU                       CAUCAU     //     CAUCAUCAUC         //
-			                        CAUCAUCAU                          CAUCAUCAUCA                    CAUCAUC     //    AUCAUCAUC           //
-			                   CAUCAUCAUCAUCAUCA                    AUCAUCAUCAUCAU                   CAUCAUC      /-//-//-//-//-//-//-//-//-//
-		                    CAUCAUCAUCAUCAUCAUCAU                 CAUCAUCAUCAUCAUC                 AUCAUCAUC        AUCAUCAUCAU
-		                 CAUCAUCAUCAUCAUCAUCAUCA               UCAUCAUCA    UCAUCA        CAUCAU  UCAUCAUCA       UCAUCAUCAUCA            UC
-		              CAUCAUCAUCAUCAUCAUCAUCAU               CAUCAUCAUC    AUCAUC   CAUCAUCAUC   CAUCAUCAU     CAUCAUCAUCAUCAU         CAUC
-		             CAUCAUCAUCAUCAUCAUCAU                  CAUCAUCAUC    AUCAUCAUCAUCAUCAU      CAUCAUCA    UCAUCAU CAUCAUCAU    CAUCAUC
-	               CAUCAUCAUCAUCA                          UCAUCAUCA     UCAUCAUCAUCAUCAU       UCAUCAUCA UCAUCAUCA   UCAUCAUCAUCAUCAU
-	              CAUCAUCAUCAUC                           AUCAUCAU  CAUCAUCAUCAUCAUCA           AUCAUCAUCAUCAUCA        UCAUCAUCAUCAUC
-	             CAUCAUCAUCAU                    CA     UCAUCAUCAUCAUCAUCAUCAUCAUC               AUCAUCAUCAUC             AUCAUCA
+                                                                                                                  //        CAUCAUCA        //
+                                                                                                         CAU      //       CAUCAUCAUC       //
+                                                                          CAUCAU                       CAUCAU     //     CAUCAUCAUC         //
+                                    CAUCAUCAU                          CAUCAUCAUCA                    CAUCAUC     //    AUCAUCAUC           //
+                               CAUCAUCAUCAUCAUCA                    AUCAUCAUCAUCAU                   CAUCAUC      /-//-//-//-//-//-//-//-//-//
+                            CAUCAUCAUCAUCAUCAUCAU                 CAUCAUCAUCAUCAUC                 AUCAUCAUC        AUCAUCAUCAU
+                         CAUCAUCAUCAUCAUCAUCAUCA               UCAUCAUCA    UCAUCA        CAUCAU  UCAUCAUCA       UCAUCAUCAUCA            UC
+                      CAUCAUCAUCAUCAUCAUCAUCAU               CAUCAUCAUC    AUCAUC   CAUCAUCAUC   CAUCAUCAU     CAUCAUCAUCAUCAU         CAUC
+                     CAUCAUCAUCAUCAUCAUCAU                  CAUCAUCAUC    AUCAUCAUCAUCAUCAU      CAUCAUCA    UCAUCAU CAUCAUCAU    CAUCAUC
+                   CAUCAUCAUCAUCA                          UCAUCAUCA     UCAUCAUCAUCAUCAU       UCAUCAUCA UCAUCAUCA   UCAUCAUCAUCAUCAU
+                  CAUCAUCAUCAUC                           AUCAUCAU  CAUCAUCAUCAUCAUCA           AUCAUCAUCAUCAUCA        UCAUCAUCAUCAUC
+                 CAUCAUCAUCAU                    CA     UCAUCAUCAUCAUCAUCAUCAUCAUC               AUCAUCAUCAUC             AUCAUCA
                  CAUCAUCAUCA                CAUC       AUCAUCAUCAUCAUCAUCAUCAUCA                  AUCAUCAU
-	             CAUCAUCAUCAU         CAUCAUC        AUCAUCAUCAUCAUCAUC AUCAUCAU
-	              CAUCAUCAUCAUCAUCAUCAUCAUC      AUCAUCAUCAUCAUCA       UCAUCAU
-	               CAUCAUCAUCAUCAUCAUCAU    CAUCAUCAUCAUCAUC             AUCAU
-	                  CAUCAUCAUCAU        CAUCAUCAUCAUCAUCA             UCAUCA
-		                                        CAUCAUCAU                CAUC
+                 CAUCAUCAUCAU         CAUCAUC        AUCAUCAUCAUCAUCAUC AUCAUCAU
+                  CAUCAUCAUCAUCAUCAUCAUCAUC      AUCAUCAUCAUCAUCA       UCAUCAU
+                   CAUCAUCAUCAUCAUCAUCAU    CAUCAUCAUCAUCAUC             AUCAU
+                      CAUCAUCAUCAU        CAUCAUCAUCAUCAUCA             UCAUCA
+                                                CAUCAUCAU                CAUC
                                                CAUCAUCAU                 CAU
                                                 CAUCAU                    CA
                                                                            C
